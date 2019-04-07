f3ac1f8beb2747f2a61dbb7177c92fa460842442
amukherj <arindam.mukerjee@gmail.com>
2019-04-07 11:30:17 +0530

    Removing unnecessary types, cleaning up code.

--- src/ch01/meetup/go/higher_order.go
+++ src/ch01/meetup/go/higher_order.go
@@ -3,13 +3,6 @@ package main
 import "fmt"
 
 type Unary func(float64) float64
-type UnaryMaker func(float64) Unary
-
-// Functional refactorings:
-//   Tennet principle
-//   Binding
-//   Wrap function
-//   Inline definition
 
 func main() {
 	compose := func(f Unary, g Unary) Unary {
@@ -18,17 +11,17 @@ func main() {
 		})
 	}
 
-	make_adder := UnaryMaker(func(n float64) Unary {
+	make_adder := func(n float64) Unary {
 		return Unary(func(x float64) float64 {
 			return x + n
 		})
-	})
+	}
 
-	make_multiplier := UnaryMaker(func(n float64) Unary {
+	make_multiplier := func(n float64) Unary {
 		return Unary(func(x float64) float64 {
 			return x * n
 		})
-	})
+	}
 
 	fmt.Println(compose(make_multiplier(3), make_adder(1))(10))
 }
